e68af6130d35554ed80ac4662f62c7e9c2d96b08
Chernikov Andrey <chernikov@intervolga.ru>
2024-03-31 18:51:50 +0300

    add telegram script

---
+++ a.py
@@ -0,0 +1,17 @@
+import telebot
+import json 
+
+def get_token():
+    token = ''
+    with open('token.json') as file:
+        json_answer = json.load(file)
+        token = json_answer['config']
+    return token
+
+TOKEN = get_token()
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def send_start(message):
+    bot.send_message(message.from_user.id, 'Привет-пока')
+
+bot.polling(none_stop=True, interval=0, timeout=120)
